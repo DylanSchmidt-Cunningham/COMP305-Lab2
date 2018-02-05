17b25880d4e5324619f9dd77111177c599cf6cb1
DylanSchmidt-Cunningham <da_cunningham@laurentian.ca>
2018-02-04 23:30:09 -0500

    added script and triggers

---
+++ Assets/buttonClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonClick : MonoBehaviour {
+
+    [SerializeField]
+	public GameObject dylanSprite;
+    public Sprite newSprite;
+
+    private SpriteRenderer sRend;
+
+	// Use this for initialization
+	void Start () {
+		sRend = dylanSprite.GetComponent<SpriteRenderer>();
+	}
+	
+	// Executes when a mouse down event is fired on the attached object
+    void OnMouseDown()
+    {
+        sRend.sprite = newSprite;
+    }
+}
